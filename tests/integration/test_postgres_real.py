d67a88d13e5494fdb26cf7a9950bb26418aa4f89
UGREEN USER <dev_market@debian.dedomain>
2026-09-23 21:31:09 +0200

    test(pr-111): assert complete postgres raw schema

--- tests/integration/test_postgres_real.py
+++ tests/integration/test_postgres_real.py
@@ -25,6 +25,7 @@ from application.postgres_sync import (
     POSTGRES_CONSUMER_SCHEMA,
     POSTGRES_CONSUMER_TABLE,
     POSTGRES_DATASET_ID,
+    POSTGRES_FED_RAW_COLUMNS,
     POSTGRES_RAW_COLUMNS,
     GoldDeltaPlan,
     GoldRowDigest,
@@ -273,7 +274,10 @@ def test_real_postgres_migrations_are_idempotent_and_round_trip(
         migrations = connection.execute(
             "SELECT version FROM macro_loader_sync.schema_migrations ORDER BY version"
         ).fetchall()
-    assert {column[0] for column in columns} == set(POSTGRES_RAW_COLUMNS)
+    assert {column[0] for column in columns} == {
+        *POSTGRES_RAW_COLUMNS,
+        *POSTGRES_FED_RAW_COLUMNS,
+    }
     assert migrations == [(version,) for version in range(1, len(postgres_module._MIGRATIONS) + 1)]
 
     timestamp = _timestamp(20)
